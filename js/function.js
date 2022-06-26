bc5a60e081f37c107dfce8834c33ab9816c16176
Davit Gelashvili <datoogelashvili@gmail.com>
2022-06-26 23:35:40 +0400

    edit code

--- js/function.js
+++ js/function.js
@@ -1,70 +1,82 @@
-const LoadingInterval = setInterval(Loading, 100)
-let loadingNumber = 0;
-function Loading(){
-    loadingNumber += 1;
+var loadingNumber = 0;
+for (loadingNumber; loadingNumber <= 24; loadingNumber++) {
     if(loadingNumber > 9) {
-        $('.loading__img').attr('src', `./img/loading/Intro_000${loadingNumber}.png`)
-        if(loadingNumber == 24){
-            clearInterval(LoadingInterval);
-            coverImage()
-            $('.avatar__cover').addClass('active')
-            $('.loading').remove();
-        }
+        $('.loading').append(`
+            <img src="./img/loading/Intro_000${loadingNumber}.png" class="loading__img ${loadingNumber}" alt="">
+        `)
+
     }else {
-        $('.loading__img').attr('src', `./img/loading/Intro_0000${loadingNumber}.png`)
-    }
-}
-Loading();
-
-let coverNumber = 107;
-function coverImage(){
-    console.log(coverNumber)
-    coverNumber += 1;
-
-    if(coverNumber == 213){
-        coverNumber = 107;
-    }
-
-    // if menu hover 
-    if( $('.menu__item--text').hasClass('glitch') ){
-        $('.avatar__cover').attr('src', `./img/Work_Idle/Work_Idle_00${coverNumber}.png`)
-    }else {
-        $('.avatar__cover').attr('src', `./img/idle/Idle_00${coverNumber}.png`)
+        $('.loading').append(`
+            <img src="./img/loading/Intro_0000${loadingNumber}.png" class="loading__img ${loadingNumber}" alt="">
+        `)
     }
 }
 
-const coverInterval = setInterval(coverImage, 100) 
+var coverNumber = 107;
+for (coverNumber; coverNumber <= 213; coverNumber++) {
+    $('.avatar.default').append(`
+        <img src="./img/native/Idle_00${coverNumber}.png" alt="" class="avatar__cover default__cover ${coverNumber}">
+    `)
+    $('.avatar.hover').append(`
+        <img src="./img/hover/Work_Idle_00${coverNumber}.png" alt="" class="avatar__cover hover__cover ${coverNumber}">
+    `)
+}
 
-$('.menu__item--text').on("mouseenter", function(){
+$( document ).ready(function() {
     
-})
+    function mainStart(){
+        const LoadingInterval = setInterval(Loading, 100)
+
+        let loadingNumber = 0;
+        function Loading (){
+            $(`.loading__img`).hide()
+            loadingNumber += 1;
+            $(`.loading__img.${loadingNumber}`).show()
+            if(loadingNumber == 24){
+                clearInterval(LoadingInterval);
+                setInterval(coverImage, 100)
+                $('.loading').remove();
+            }
+        }
+
+        let coverNumber = 107;
+        function coverImage(){
+            $(`.avatar__cover`).hide()
+            coverNumber += 1;
+
+            // if menu hover 
+            if( $('.menu__item--text').hasClass('glitch') ){
+                $(`.avatar__cover.hover__cover.${coverNumber}`).show()
+            }else {
+                $(`.avatar__cover.default__cover.${coverNumber}`).show()
+            }
+
+            if(coverNumber == 213){
+                coverNumber = 107;
+            }
+        }
+    }
+    mainStart()
+});
 
 
 
 
+    $('.open-second-page').on("click", function(){
+        $('.main').removeClass('active');
+        $('.second').addClass('active');
+    })
+    $('.second-close').on("click", function(){
+        $('.main').addClass('active');
+        $('.second').removeClass('active');
+    })
 
-
-
-
-
-
-
-
-$('.open-second-page').on("click", function(){
-    $('.main').removeClass('active');
-    $('.second').addClass('active');
-})
-$('.second-close').on("click", function(){
-    $('.main').addClass('active');
-    $('.second').removeClass('active');
-})
-
-$('.menu__item--text').on("mouseenter", function(){
-    $(this).addClass('glitch');
-})
-$('.menu__item--text').on("mouseleave", function(){
-    $('.menu__item--text').removeClass('glitch');
-})
+    $('.menu__item--text').on("mouseenter", function(){
+        $(this).addClass('glitch');
+    })
+    $('.menu__item--text').on("mouseleave", function(){
+        $('.menu__item--text').removeClass('glitch');
+    })
 
 
 
