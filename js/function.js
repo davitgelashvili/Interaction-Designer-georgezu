15918dc1dbb262f208bdf68c847ebce3c22cbb81
Davit Gelashvili <datoogelashvili@gmail.com>
2022-07-26 21:54:09 +0400

    about effect

--- js/function.js
+++ js/function.js
@@ -13,8 +13,15 @@ $(window).on('load', function(){
 
         // if menu hover 
         if( $('.menu__item--text').hasClass('glitch') ){
-            $(`.avatar__cover.hover__cover.${coverNumber}`).show()
-            $(`.avatar__cover.default__cover.${coverNumber}`).show()
+            $(`.avatar__cover.default__cover.${coverNumber}`).show();
+
+            if($('.menu__item--work').hasClass('active')){
+                $(`.avatar__cover.hover__cover.work.${coverNumber}`).show()
+            }
+
+            if($('.menu__item--about').hasClass('active')){
+                $(`.avatar__cover.hover__cover.about.${coverNumber}`).show()
+            }
         }else {
             $(`.avatar__cover.default__cover.${coverNumber}`).show()
             $(`.avatar__cover.skins__cover.${coverNumber}`).show()
@@ -70,22 +77,33 @@ $('.second-close').on("click", function(){
         $(`.second__avatar--cover`).removeClass('active');
         $(`.second__avatar--cover.${loadingNumber}`).addClass('active')
         $(`.second__avatar`).removeClass('active')
-        if(loadingNumber == 310){
+        if(loadingNumber == 309){
+            clearInterval(LoadingInterval);
             $('.main').addClass('active');
             $('.second').removeClass('active');
             $('.avatar').show();
-            clearInterval(LoadingInterval);
         }
     }
 })
 
+// menu glich effect 
 $('.menu__item--text').on("mouseenter", function(){
     $(this).addClass('glitch');
 })
-$('.menu__item--text').on("mouseleave", function(){
-    $('.menu__item--text').removeClass('glitch');
+
+// hover effect
+$('.menu__item--work').on("mouseenter", function(){
+    $(this).addClass('active')
+})
+$('.menu__item--about').on("mouseenter", function(){
+    $(this).addClass('active')
 })
 
+// delete effect
+$('.menu__item--text').on("mouseleave", function(){
+    $('.menu__item--text').removeClass('glitch');
+    $('.menu__item--text').removeClass('active');
+})
 
 function postedWork(){
     const token = 'd820991ca43cc815adf1a0a4a2e08a';
